11d5fdce8663428dcb8cff92ed056adaac06ddae
Győző Papp <gyozo.papp@meltwater.com>
2017-06-01 20:45:12 +0200

    clean up a bit.

--- companyscraping/spiders/linkextractor.py
+++ companyscraping/spiders/linkextractor.py
@@ -1,8 +1,11 @@
 import csv
-import csv
 from scrapy import Spider, Request
 from scrapy.linkextractors import LinkExtractor
 from pkg_resources import resource_filename
+
+
+COMPANY_FILE = "resources/fhai_company_urls.csv"
+
 
 class LinkSpider(Spider):
     name = "link-extractor"
@@ -12,15 +15,13 @@ class LinkSpider(Spider):
         'DEPTH_PRIORITY': 1
     }
 
-    COMPANY_FILE = "resources/fhai_company_urls.csv"
-
     def __init__(self):
         super(LinkSpider, self).__init__()
         self.extractor = LinkExtractor()
 
     def start_requests(self):
         requests = []
-        with open(resource_filename('companyscraping', LinkSpider.COMPANY_FILE), 'rt') as csvfile:
+        with open(resource_filename('companyscraping', COMPANY_FILE), 'rt') as csvfile:
             for row in csv.DictReader(csvfile):
                 requests.append(Request(url=row['website'], meta=row, callback=self.parse))
         return requests
