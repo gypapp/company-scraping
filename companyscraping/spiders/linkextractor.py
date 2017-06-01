e77f1fc4063e8c3125343c8b28efbb0146c160d1
Győző Papp <gyozo.papp@meltwater.com>
2017-06-01 19:42:16 +0200

    maxdepth=1 link extraction from ORB 2017 ranked company websites

--- companyscraping/spiders/linkextractor.py
+++ companyscraping/spiders/linkextractor.py
@@ -1,18 +1,44 @@
+import csv
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 
 
 class LinkSpider(scrapy.Spider):
     name = "link-extractor"
-    allow_domains = 'tiempodev.com'
+
     start_urls = [
-        'http://www.tiempodev.com/index',
+        "http://www.straine.com/"
     ]
 
+    custom_settings = {
+        'DEPTH_LIMIT': 1,
+        'DEPTH_PRIORITY': 1
+    }
+
+    COMPANIES = 'fhai_company_urls-head.csv'
+
+    def __init__(self):
+        super(LinkSpider, self).__init__()
+        self.extractor = LinkExtractor()
+
+    def start_requests(self):
+        requests = []
+        with open(LinkSpider.COMPANIES, 'rt') as csvfile:
+            for row in csv.DictReader(csvfile):
+                requests.append(scrapy.Request(url=row['website'], meta=row, callback=self.parse))
+        return requests
+
     def parse(self, response):
-        extractor = LinkExtractor(allow_domains=self.allow_domains)
-        for link in extractor.extract_links(response):
-            yield {
-                'url': link.url,
-                'text': link.text
-            }
+        yield self._create_item(response.meta, response.url)
+        for link in self.extractor.extract_links(response):
+            yield self._create_item(response.meta, link.url, link.text)
+            # make a request to the page (DEPTH_LIMIT may discard it)
+            yield scrapy.Request(url=link.url, meta=response.meta, callback=self.parse)
+
+    def _create_item(self, meta, url, text=''):
+        return {
+            'orb_num': meta['orb_num'],
+            'company': meta['name'],
+            'url': url,
+            'text': text
+        }
