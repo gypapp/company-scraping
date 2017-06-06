a4f3c50897d85188a0459158ff8c3a72b59939f7
Győző Papp <gyozo.papp@meltwater.com>
2017-06-06 13:40:18 +0200

    use tldextract for domain filtering

--- companyscraping/settings.py
+++ companyscraping/settings.py
@@ -14,6 +14,7 @@ BOT_NAME = 'companyscraping'
 SPIDER_MODULES = ['companyscraping.spiders']
 NEWSPIDER_MODULE = 'companyscraping.spiders'
 
+DUPEFILTER_CLASS = 'scrapy.dupefilters.RFPDupeFilter'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'companyscraping (+http://www.yourdomain.com)'
@@ -22,12 +23,12 @@ NEWSPIDER_MODULE = 'companyscraping.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -50,6 +51,7 @@ ROBOTSTXT_OBEY = True
 #    'companyscraping.middlewares.CompanyscrapingSpiderMiddleware': 543,
 #}
 
+# Enable or disable downloader middlewares
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
--- companyscraping/spiders/linkextractor.py
+++ companyscraping/spiders/linkextractor.py
@@ -2,7 +2,7 @@ import csv
 from scrapy import Spider, Request
 from scrapy.linkextractors import LinkExtractor
 from pkg_resources import resource_filename
-
+import tldextract
 
 COMPANY_FILE = "resources/fhai_company_urls.csv"
 
@@ -15,31 +15,36 @@ class LinkSpider(Spider):
         'DEPTH_PRIORITY': 1
     }
 
-    def __init__(self):
-        super(LinkSpider, self).__init__()
-        self.extractor = LinkExtractor()
-
     def start_requests(self):
         requests = []
         with open(resource_filename('companyscraping', COMPANY_FILE), 'rt') as csvfile:
             for row in csv.DictReader(csvfile):
-                requests.append(Request(url=row['website'], meta=row, callback=self.parse))
+                row['seed'] = True
+                requests.append(Request(url=row['website'], meta=row, callback=self.parse_link_canonical))
         return requests
 
+    def parse_link_canonical(self, response):
+        canonical = response.xpath('//link[@rel="canonical"]/@href').extract_first()
+        yield { 'url': response.url,
+                'canonical': canonical }
+
     def parse(self, response):
-        # remove if you don't need the seed url
-        if 'text' not in response.meta:
+        if response.meta['seed']:
             yield self._create_item(response.meta, response.url)
-        for link in self.extractor.extract_links(response):
-            # yield all the links as a result item to pipeline
+
+        for link in self.links_from_domain(response):
             yield self._create_item(response.meta, link.url, link.text)
-            # make a request to the page (DEPTH_LIMIT may discard it)
             yield Request(url=link.url, meta=response.meta, callback=self.parse)
 
+    def links_from_domain(self, response):
+        domain = '.'.join(tldextract.extract(response.url)[-2:])
+        return LinkExtractor(allow=[domain]).extract_links(response)
+
     def _create_item(self, meta, url, text=''):
+        meta['seed'] = False
         return {
             'orb_num': meta['orb_num'],
             'company': meta['name'],
-            'url': url,
-            'text': text
+            'url': url.strip(),
+            'text': text.strip()
         }
